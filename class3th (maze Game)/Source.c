6cec083935b6156f95217d1ab0b9f357353de205
hachandl <hachandl12341@gmail.com>
2024-09-20 21:45:20 +0900

    (2024.9.20) 파일 입출력

--- class3th (maze Game)/Source.c
+++ class3th (maze Game)/Source.c
@@ -1,7 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+#define SIZE 100000
+
+void Load1(const char* Valstrax1)
+{
+    FILE* file = fopen(Valstrax1, "r");
+
+    char buffer[SIZE] = { 0, };
+
+    fread(buffer, 1, SIZE, file);
+
+    printf("%s", buffer);
+
+    fclose(file);
+}
+void Load2(const char* Valstrax2)
+{
+    FILE* file = fopen(Valstrax2, "r");
+
+    char buffer[SIZE] = { 0, };
+
+    fread(buffer, 1, SIZE, file);
+
+    printf("%s", buffer);
+
+    fclose(file);
+}
+void Load3(const char* Valstrax3)
+{
+    FILE* file = fopen(Valstrax3, "r");
+
+    char buffer[SIZE] = { 0, };
+
+    fread(buffer, 1, SIZE, file);
+
+    printf("%s", buffer);
+
+    fclose(file);
+}
 
 int main()
 {
+#pragma region 파일 입출력
 
-	return 0;
+#pragma region 파일 쓰기
+
+    // 첫 번째 매개변수 (파일의 이름)
+    // 두 번째 매개변수 (파일의 입출력 모드)
+    // FILE * file = fopen("data.txt", "w");
+    // 
+    // fputs("Character Information\n", file);
+    // fputs("Health : \n", file);
+    // fputs("Attack : \n", file);
+    // fputs("Defense : \n", file);
+    // 
+    // fclose(file);
+
+#pragma endregion
+
+#pragma region 파일 읽기
+
+    Load1("Valstrax1.txt");
+
+
+#pragma endregion
+
+
+
+
+#pragma endregion
+
+
+
+    return 0;
 }
